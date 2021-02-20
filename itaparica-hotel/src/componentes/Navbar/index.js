de2b276ea9bc904022b8946bb31d28ab82cf7f4c
deko possas <andrepossaster@gmail.com>
2021-02-20 20:20:57 -0300

    ta saindo de mais essa nav bar

--- itaparica-hotel/src/componentes/Navbar/index.js
+++ itaparica-hotel/src/componentes/Navbar/index.js
@@ -3,24 +3,41 @@ import styled from 'styled-components';
 import logoHeader from './imagens/logoHeader.png';
 
 const Nav = styled.nav`
-  top: 13px;
-  left: 42px;
-  width: 1283px;
+  width: 90%;
   height: 74px;
   background: #ffffff 0% 0% no-repeat padding-box;
   box-shadow: 0px 3px 16px #0000001a;
   border-radius: 8px;
   opacity: 0.98;
-`;
-const LogoHeader = styled.a`
-  width: 62px;
-  height: 65px;
-  opacity: 1;
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px 5%;
 `;
 
-const Button1 = styled.a`
+const NavButton = styled.nav`
+  display: flex;
+  margin: 7%;
+`;
+
+
+const LogoHeader = styled.a`
+  opacity: 1;
+  margin: 7%
+`;
+
+const ImageLogo = styled.img`
+  width: 62px;
+  height: 65px;
+`;
+
+const Button1 = styled.li`
   opacity: 0.9;
-  hover: 1;
+  cursor: pointer;
+  color: #9A9A9A;
+  list-style: none;
+  margin: 20px
 `;
 
 const Button2 = styled.button``;
@@ -29,15 +46,17 @@ const NavBar = () => {
   return (
     <Nav>
       <LogoHeader href="#">
-        <img src={logoHeader} alt="hotel itaparica"></img>
+        <ImageLogo src={logoHeader} alt="hotel itaparica"></ImageLogo>
       </LogoHeader>
-      <Button1 href="#">Inicio</Button1>
-      <Button1>quartos</Button1>
-      <Button1>o hotal</Button1>
-      <Button1>promoção</Button1>
-      <Button1>localização</Button1>
-      <Button1>contato</Button1>
-      <Button2>Reservas</Button2>
+      <NavButton>
+        <Button1 href="#">Inicio</Button1>
+        <Button1>quartos</Button1>
+        <Button1>o hotal</Button1>
+        <Button1>promoção</Button1>
+        <Button1>localização</Button1>
+        <Button1>contato</Button1>
+        <Button2>Reservas</Button2>
+      </NavButton>
     </Nav>
   );
 };
