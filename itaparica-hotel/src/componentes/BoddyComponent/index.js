4587c43963df690bdf7701c7ee9989312f258d7f
deko possas <andrepossaster@gmail.com>
2021-02-21 00:02:58 -0300

    tentando centralizar essa foto

--- itaparica-hotel/src/componentes/BoddyComponent/index.js
+++ itaparica-hotel/src/componentes/BoddyComponent/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import styled from 'styled-components';
 import cofeBreakerBackGround from './image3/Image3.png';
 
-const Boddy = styled.div``;
-
-const BackgroundImage = styled.img`
+const Boddy = styled.div`
   background-size: cover;
-  min-height: 100px;
+  background:url(${cofeBreakerBackGround}); 
+  background-repeat: no-repeat;
+  min-height: 100vh;
 `;
 
 const BoddyComponent = () => {
   return <Boddy>
-    <BackgroundImage src={cofeBreakerBackGround} alt="sala de cafe"></BackgroundImage>
   </Boddy>;
 };
 
