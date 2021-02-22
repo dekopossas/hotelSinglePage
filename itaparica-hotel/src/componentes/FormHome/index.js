47635bfcfd2f1fb65597bbd0c4084cf3f56262a9
deko possas <andrepossaster@gmail.com>
2021-02-21 23:40:21 -0300

    forms comecado

--- itaparica-hotel/src/componentes/FormHome/index.js
+++ itaparica-hotel/src/componentes/FormHome/index.js
@@ -1,21 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
+import logoForm from './logoDesktop/logo desktop.png'
 
 const Form = styled.div`
   top: 182px;
   left: 306px;
   width: 272px;
   height: 403px;
-  background: #FFFFFF 0% 0% no-repeat padding-box;
-  box-shadow: 0px 3px 26px #947B5F74;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 26px #947b5f74;
   border-radius: 11px;
   position: absolute;
   opacity: 1;
+  display: flex;
+  flex-flow: column wrap;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonReseveAgora = styled.button`
+  background: #f16136 0% 0% no-repeat padding-box;
+  border-radius: 19px;
+  opacity: 1;
+  height: 37px;
+  width: 242px;
+
+`;
+
+const LogoHotel = styled.img`
+  width: 130px;
+  height: 72px;
 `;
 
 const FormHome = () => {
-  return <Form>
-  </Form>;
+  return (
+    <Form>
+      <LogoHotel src={logoForm} alt="logo do hotel"></LogoHotel>
+      <ButtonReseveAgora>Reserve agora</ButtonReseveAgora>
+    </Form>
+  );
 };
 
 export default FormHome;
